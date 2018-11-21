8c33c6fa122b11c7ab49c311359a9b1851841488
William Di Luigi <williamdiluigi@gmail.com>
2018-11-21 16:30:18 +0100

    updates

--- App.js
+++ App.js
@@ -6,22 +6,89 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import { StyleSheet, Text, Image, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, Image, View, FlatList, Button } from 'react-native';
+// import { point, distance } from '@turf/turf';
+import { PermissionsAndroid } from 'react-native';
 
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true
+    };
+  }
+
+  async requestLocation() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'location',
+          'message': 'location request'
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the location")
+      } else {
+        console.log("Location permission denied")
+      }
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+
+  componentDidMount() {
+    console.log("mounted");
+
+    this.requestLocation();
 
 
-export default class App extends Component{
+    // navigator.geolocation.requestAuthorization();
+
+    // navigator.geolocation.getCurrentPosition((pos) => {
+    //   console.log(pos);
+    // });
+
+    fetch("https://ranky.olinfo.it")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          isLoading: false,
+          eventList: data
+        })
+      })
+  }
+
+  dist(lat, lon) {
+    return distance(
+        point(navigator.geolocation.getCurrentPosition()),
+        point([lat, lon])
+    );
+  }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>Let Start</Text>
-        <Image style={styles.img} source={{uri:"https://arnabdey.co/uploads/1/2/2/6/122619697/img-3152_orig.jpg"}}/>
-      </View>
-    );
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>Let Start</Text>
+          <Image style={styles.img} source={{uri:"https://arnabdey.co/uploads/1/2/2/6/122619697/img-3152_orig.jpg"}}/>
+        </View>
+      );
+    } else {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <FlatList data={this.state.eventList}
+              renderItem={({item}) => <Button title={item.name + ', dist: ~'
+                // + this.dist(item.latitude, item.longitude)
+              }/>}
+              keyExtractor={({id}) => id} />
+        </View>
+      );
+    }
   }
 }
 
