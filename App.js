727b144edab8e6383626b0e6e993817c5c247da7
William Di Luigi <williamdiluigi@gmail.com>
2018-11-21 17:18:07 +0100

    distance works

--- App.js
+++ App.js
@@ -8,7 +8,6 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, Image, View, FlatList, Button } from 'react-native';
-// import { point, distance } from '@turf/turf';
 import { PermissionsAndroid } from 'react-native';
 
 
@@ -27,30 +26,35 @@ export default class App extends Component {
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         {
           'title': 'location',
-          'message': 'location request'
+          'message': 'The location is required to show the contests in order of distance'
         }
       )
+
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("You can use the location")
+        navigator.geolocation.getCurrentPosition((pos) => {
+          this.setState({
+            location: pos.coords
+          });
+        });
       } else {
-        console.log("Location permission denied")
+        this.setState({
+          location: null
+        });
       }
     } catch (err) {
-      console.warn(err)
+      console.warn(err);
+
+      this.setState({
+        location: null
+      });
     }
   }
 
   componentDidMount() {
     console.log("mounted");
 
-    this.requestLocation();
-
-
-    // navigator.geolocation.requestAuthorization();
-
-    // navigator.geolocation.getCurrentPosition((pos) => {
-    //   console.log(pos);
-    // });
+    // request location if needed, and then re-render
+    this.requestLocation()
 
     fetch("https://ranky.olinfo.it")
       .then((response) => response.json())
@@ -62,11 +66,22 @@ export default class App extends Component {
       })
   }
 
-  dist(lat, lon) {
-    return distance(
-        point(navigator.geolocation.getCurrentPosition()),
-        point([lat, lon])
-    );
+  dist(lat2, lon2) {
+    var lat1 = this.state.location.latitude;
+    var lon1 = this.state.location.longitude;
+
+    var R = 6371e3; // metres
+    var φ1 = lat1 * Math.PI / 180;
+    var φ2 = lat2 * Math.PI / 180;
+    var Δφ = (lat2-lat1) * Math.PI / 180;
+    var Δλ = (lon2-lon1) * Math.PI / 180;
+
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return (R * c / 1000).toFixed(2);
   }
 
   render() {
@@ -83,7 +98,7 @@ export default class App extends Component {
         <View style={{flex: 1, paddingTop: 20}}>
           <FlatList data={this.state.eventList}
               renderItem={({item}) => <Button title={item.name + ', dist: ~'
-                // + this.dist(item.latitude, item.longitude)
+                  + this.dist(item.latitude, item.longitude) + 'km'
               }/>}
               keyExtractor={({id}) => id} />
         </View>
