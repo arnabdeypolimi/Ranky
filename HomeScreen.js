082c55ce80e7ed46b8d0eac78e70606e0aeb2e99
William Di Luigi <williamdiluigi@gmail.com>
2018-12-20 15:54:30 +0100

    minor

--- HomeScreen.js
+++ HomeScreen.js
@@ -77,13 +77,13 @@ export default class HomeScreen extends React.Component {
     var R = 6371e3; // metres
     var φ1 = lat1 * Math.PI / 180;
     var φ2 = lat2 * Math.PI / 180;
-    var Δφ = (lat2-lat1) * Math.PI / 180;
-    var Δλ = (lon2-lon1) * Math.PI / 180;
+    var Δφ = (lat2 - lat1) * Math.PI / 180;
+    var Δλ = (lon2 - lon1) * Math.PI / 180;
 
     var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
             Math.cos(φ1) * Math.cos(φ2) *
             Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     return (R * c / 1000).toFixed(2);
   }
