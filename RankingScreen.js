d2224d27ff3fb9e7becd77e44cb925c312bdb7c1
William Di Luigi <williamdiluigi@gmail.com>
2019-01-16 20:43:33 +0100

    fix

--- RankingScreen.js
+++ RankingScreen.js
@@ -18,6 +18,7 @@ export default class RankingScreen extends Component {
   }
 
   componentDidMount() {
+    const { navigation } = this.props;
     const contest = navigation.getParam("contest");
 
     // subscribe to the topic
